991d65bc7d11a255fa40b16c320bb6d4ad5cf49f
Cassidy Pignatello <cassidy.pignatello@gmail.com>
2025-02-25 18:14:47 +0800

    formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx'
-import "./styles/global.css";
+import './styles/global.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
